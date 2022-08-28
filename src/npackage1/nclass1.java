d4f1ceb9282e3d721c390606e9ee2a10f471006c
akhilawarrier123 <akhilawarrier5@gmail.com>
2022-08-28 12:23:51 +0530

    modified one

--- src/npackage1/nclass1.java
+++ src/npackage1/nclass1.java
@@ -5,6 +5,7 @@ public class nclass1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("Welcome to Java");
+	System.out.println("modified version");
 
 	}
 
